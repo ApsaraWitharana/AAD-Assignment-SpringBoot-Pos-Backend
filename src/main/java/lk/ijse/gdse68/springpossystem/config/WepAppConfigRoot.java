77da790f367d5e3aebe48e8354e33cb0882ac2de
ApsaraWitharana <asachini095@gmail.com>
2024-10-04 12:35:20 +0530

    update - webapp config

--- src/main/java/lk/ijse/gdse68/springpossystem/config/WepAppConfigRoot.java
+++ src/main/java/lk/ijse/gdse68/springpossystem/config/WepAppConfigRoot.java
@@ -1,4 +1,21 @@
 package lk.ijse.gdse68.springpossystem.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.gdse68.springpossystem")
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class WepAppConfigRoot {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
